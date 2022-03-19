87c8553a3e80110ad63062d98625eb068d5b682f
mingyuchoo <mingyuchoo@gmail.com>
2022-03-19 14:45:30 +0900

    added

---
+++ 10-scripts/default-function-version.nix
@@ -0,0 +1,42 @@
+# default.nix
+
+## function version
+{ pkgs ? import <nixpkgs> {}
+} :
+let
+    version = "ghc921";
+in
+    with pkgs;
+        stdenv.mkDerivation rec {
+            name = "<project-name>";
+            src = ./.;
+            buildInputs = [
+                haskell.compiler."${version}"
+                cabal-install
+                stack
+                ghcid
+            ];
+            shellHook = ''
+                # execute `stack run` command
+                stack run
+            '';
+        }
+
+## let ... in ... expresion vesion
+#let
+#    pkgs = import <nixpkgs> {};
+#    version = "ghc921";
+#in
+#    with pkgs;
+#    mkShell {
+#        buildInputs =  [
+#            haskell.compiler."${version}"
+#            cabal-install
+#            stack
+#            ghcid
+#        ];
+#        shellHook = ''
+#            # execute `stack run` command
+#            stack run
+#        '';
+#    }
