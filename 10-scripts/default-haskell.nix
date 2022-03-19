6ae0fb2859c4d5e4a4ea311e9bce9368d8796baf
mingyuchoo <mingyuchoo@gmail.com>
2022-03-19 15:27:03 +0900

    updated

---
+++ 10-scripts/default-haskell.nix
@@ -0,0 +1,65 @@
+# default.nix
+
+## 1) with expresion version
+################################################
+with import <nixpkgs> {};
+    let
+        version = "ghc921";
+    in
+            stdenv.mkDerivation rec {
+                name = "<project-name>";
+                src = ./.;
+                buildInputs = [
+                    haskell.compiler."${version}"
+                    cabal-install
+                    stack
+                    ghcid
+                ];
+                shellHook = ''
+                    # execute `stack run` command
+                    stack run
+                '';
+            }
+
+### 2) function version
+################################################
+#{ pkgs ? import <nixpkgs> {}
+#} :
+#let
+#    version = "ghc921";
+#in
+#    with pkgs;
+#        stdenv.mkDerivation rec {
+#            name = "<project-name>";
+#            src = ./.;
+#            buildInputs = [
+#                haskell.compiler."${version}"
+#                cabal-install
+#                stack
+#                ghcid
+#            ];
+#            shellHook = ''
+#                # execute `stack run` command
+#                stack run
+#            '';
+#        }
+
+## 3) let ... in ... expresion vesion
+################################################
+#let
+#    pkgs = import <nixpkgs> {};
+#    version = "ghc921";
+#in
+#    with pkgs;
+#    mkShell {
+#        buildInputs =  [
+#            haskell.compiler."${version}"
+#            cabal-install
+#            stack
+#            ghcid
+#        ];
+#        shellHook = ''
+#            # execute `stack run` command
+#            stack run
+#        '';
+#    }
