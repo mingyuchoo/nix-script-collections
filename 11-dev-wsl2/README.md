15c05bf9540535db317326d788c06be658ff5f2d
mingyuchoo <mingyuchoo@gmail.com>
2022-04-17 19:46:28 +0900

    changed

---
+++ 11-dev-wsl2/README.md
@@ -0,0 +1,17 @@
+# 01-linux-env
+
+## Prerequisite
+
+```bash
+curl -L https://nixos.org/nix/install | sh
+```
+
+## How to run
+
+```bash
+nix-shell
+
+# or
+
+nix-shell default.nix
+```
---
+++ 11-dev-wsl2/default.nix
@@ -0,0 +1,63 @@
+let
+    pkgs = import <nixpkgs> {};
+    python3-with-packages = pkgs.python3Full.withPackages (ps: with ps; [
+        pip
+        # other python packages you want
+    ]);
+in
+    pkgs.mkShell {
+        buildInputs = [
+            pkgs.cmatrix
+            pkgs.curl
+            pkgs.exa
+            pkgs.git
+            pkgs.hurl
+            pkgs.htop
+            pkgs.jq
+            pkgs.nmon
+            pkgs.nnn
+            pkgs.p7zip
+            pkgs.pgcli
+            pkgs.sysstat
+            pkgs.tmux
+            pkgs.traceroute
+            pkgs.tree
+
+            # editor
+            pkgs.emacs27-nox
+            pkgs.vim
+            pkgs.vifm
+
+            # java
+            pkgs.jdk11
+
+            # docker
+            pkgs.docker
+            pkgs.docker-client
+            pkgs.docker-compose
+            
+            # terraform
+            pkgs.terraform
+
+            # nodejs
+            pkgs.nodejs-14_x
+            pkgs.yarn
+
+            # python
+            pkgs.python2Full
+            python3-with-packages
+
+            # ansible
+            pkgs.ansible
+
+            # aws
+            pkgs.awscli2
+            pkgs.aws-sam-cli
+
+        ];
+        shellHook = ''
+            echo "---------------------------------------------"
+            echo "Great!, Nix packages were built successfully."
+            echo "---------------------------------------------"
+        '';
+}
