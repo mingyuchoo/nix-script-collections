1b3db53be915ac3996cbe8b2516016fc70dab819
mingyuchoo <mingyuchoo@gmail.com>
2022-05-12 16:36:24 +0900

    Update shell.nix

--- 11-dev-wsl2/shell.nix
+++ 11-dev-wsl2/shell.nix
@@ -1,63 +1,100 @@
+{ pkgs ? import <nixpkgs> { } }: with pkgs;
+
 let
-    pkgs = import <nixpkgs> {};
+    haskellVersion = "ghc922";
+    
     python3-with-packages = pkgs.python3Full.withPackages (ps: with ps; [
         pip
-        # other python packages you want
     ]);
+    
+    elm-with-packages = with pkgs.elmPackages; [
+        elm
+        elm-analyse
+        elm-doc-preview
+        elm-format
+        elm-live
+        elm-test
+    ];
 in
-    pkgs.mkShell {
-        buildInputs = [
-            pkgs.cmatrix
-            pkgs.curl
-            pkgs.exa
-            pkgs.git
-            pkgs.hurl
-            pkgs.htop
-            pkgs.jq
-            pkgs.nmon
-            pkgs.nnn
-            pkgs.p7zip
-            pkgs.pgcli
-            pkgs.sysstat
-            pkgs.tmux
-            pkgs.traceroute
-            pkgs.tree
+    mkShell {
+    buildInputs = [
+        pkgs.cmatrix
+        pkgs.curl
+        pkgs.exa
+        pkgs.git
+        pkgs.hurl
+        pkgs.htop
+        pkgs.jq
+        pkgs.nmon
+        pkgs.nnn
+        pkgs.p7zip
+        pkgs.pgcli
+        pkgs.sysstat
+        pkgs.tmux
+        pkgs.traceroute
+        pkgs.tree
 
-            # editor
-            pkgs.emacs28-nox
-            pkgs.vim
-            pkgs.vifm
+        # editor
+        pkgs.emacs28-nox
+        pkgs.vim
+        pkgs.vifm
 
-            # java
-            pkgs.jdk11
+        # java
+        pkgs.jdk11
 
-            # docker
-            pkgs.docker
-            pkgs.docker-client
-            pkgs.docker-compose
-            
-            # terraform
-            pkgs.terraform
+        # rust
+        pkgs.cargo
+        pkgs.rustup
 
-            # nodejs
-            pkgs.nodejs-14_x
-            pkgs.yarn
+        # haskell
+        pkgs.haskell.compiler."${haskellVersion}"
+        pkgs.haskellPackages.cabal-install
+        pkgs.haskellPackages.haskell-language-server
+        pkgs.haskellPackages.stack
+        pkgs.haskellPackages.stylish-haskell
+        pkgs.haskellPackages.ghci
+        pkgs.haskellPackages.ghcid
+        pkgs.haskellPackages.hindent
+        pkgs.haskellPackages.hlint
+        pkgs.haskellPackages.hoogle
 
-            # python
-            pkgs.python2Full
-            python3-with-packages
+        # elm
+        elm-with-packages
 
-            # ansible
-            pkgs.ansible
+        # golang
+        pkgs.go
 
-            # aws
-            pkgs.awscli2
-            pkgs.aws-sam-cli
+        # ocaml
+        pkgs.ocamlPackages.ocaml
+        pkgs.opam
 
-        ];
-        shellHook = ''
-            echo "---------------------------------------------"
-            echo "Great!, Nix packages were built successfully."
-            echo "---------------------------------------------"
-        '';
-}
+        # docker
+        pkgs.docker
+        pkgs.docker-client
+        pkgs.docker-compose
+        
+        # terraform
+        pkgs.terraform
+
+        # nodejs
+        pkgs.nodejs-16_x
+        pkgs.yarn
+
+        # python
+        pkgs.python2Full
+        python3-with-packages
+
+        # ansible
+        pkgs.ansible
+
+        # aws
+        pkgs.awscli2
+        pkgs.aws-sam-cli
+    ];
+
+    shellHook = ''
+        echo "---------------------------------------------"
+        echo "Great!, Nix packages were built successfully."
+        echo "---------------------------------------------"
+    '';
+    }
