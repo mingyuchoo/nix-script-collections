97c8ee52003d250357407eea2b751e5f75a60df3
mingyuchoo <mingyuchoo@gmail.com>
2022-05-12 16:02:06 +0900

    Update flake.nix

--- 11-dev-wsl2/flake.nix
+++ 11-dev-wsl2/flake.nix
@@ -1,1 +1,14 @@
+{
+  description = "11-dev-wsl2";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
