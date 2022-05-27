ecd67f392ba495dfd4f1e9aa3e1b974fdaa5fcfb
mingyuchoo <mingyuchoo@gmail.com>
2022-05-28 06:15:55 +0900

    fixed

--- 10-dev-linux/install.sh
+++ 10-dev-linux/install.sh
@@ -8,7 +8,7 @@ sudo apt autoremove -y
 
 # Install OpenSSH Server
 ########################################
-if [ -z $(pidof sshd) ]
+if [ -z '$(pidof sshd)' ]
 then
     sudo apt install -y openssh-server
     sudo systemctl enable ssh
@@ -20,24 +20,24 @@ fi
 
 # Install Docker
 ########################################
-if [ -z $(pidof dockerd) ]
+if [ -z '$(pidof dockerd)' ]
 then
     sudo apt install -y docker.io
 else
     echo "[NOTICE] docker daemon is already running."
 fi
 
-if [ -z $(groups | grep docker) ]
+if [ -z '$(groups | grep docker)' ]
 then
     sudo groupadd docker
-    sudo usermod -aG docker $USER
+    sudo usermod -aG docker "$USER"
 else
     echo "[NOTICE] \`docker\` group is already created."
 fi 
 
 # Install docker-compose
 ########################################
-if [ -z $(which docker-compose) ]
+if [ -z '$(which docker-compose)' ]
 then
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
@@ -47,14 +47,14 @@ fi
 
 # Create git user for Git Repository
 ########################################
-if [ -z $(egrep -e git /etc/passwd) ]
+if [ -z '$(egrep -e git /etc/passwd)' ]
 then
     sudo useradd --system --user-group --shell /bin/bash --create-home --home-dir /home/git git
 else
     echo "[NOTICE] \`git\` account is alreay exists."
 fi
 
-if [ -z $(egrep -e jenkin /etc/passwd) ]
+if [ -z '$(egrep -e jenkin /etc/passwd)' ]
 then
     sudo useradd --system --user-group --shell /bin/bash --create-home --home-dir /home/jenkins jenkins
 else
@@ -63,7 +63,7 @@ fi
 
 # Install Nix for multi-user
 ########################################
-if [ -z $(which nix) ]
+if [ -z '$(which nix)' ]
 then
     sh <(curl -L https://nixos.org/nix/install) --daemon
     #echo "source \"$HOME/.nix-profile/etc/profile.d/nix.sh\"" >> $HOME/.bashrc
@@ -73,10 +73,10 @@ fi
 
 # Install Nix Flakes
 ########################################
-if [ ! -f $HOME/.config/nix/nix.conf ]
+if [ ! -f "$HOME/.config/nix/nix.conf" ]
 then
-    mkdir -p $HOME/.config/nix && touch $HOME/.config/nix/nix.conf
-    echo "experimental-features = nix-command flakes" | tee --append $HOME/.config/nix/nix.conf > /dev/null
+    mkdir -p "$HOME/.config/nix" && touch "$HOME/.config/nix/nix.conf"
+    echo "experimental-features = nix-command flakes" | tee --append "$HOME/.config/nix/nix.conf" > /dev/null
 else
     echo "[NOTICE] Nix Flakes are already seted up."
 fi
@@ -84,9 +84,9 @@ fi
 
 # add $HOME/.emacs.d/bin path to .profile
 ########################################
-if [ -z $(egrep -e ".emacs.d/bin" $HOME/.profile) ]
+if [ -z '$(egrep -e ".emacs.d/bin" $HOME/.profile)' ]
 then
-    tee --append $HOME/.profile > /dev/null <<EOF
+    tee --append "$HOME/.profile" > /dev/null <<EOF
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "\$HOME/.emacs.d/bin" ] ; then
@@ -99,7 +99,7 @@ fi
 
 # Install Doom Emacs
 ########################################
-if [ -d $HOME/.emacs.d/bin ]
+if [ -d "$HOME/.emacs.d/bin" ]
 then
     git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
     $HOME/.emacs.d/bin/doom install
