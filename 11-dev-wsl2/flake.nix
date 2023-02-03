3a6d4f4870bfd04cf16cb8b919c5acc12c77d06a
mingyuchoo <mingyuchoo@gmail.com>
2023-02-03 17:55:27 +0900

    updated

--- 11-dev-wsl2/flake.nix
+++ 11-dev-wsl2/flake.nix
@@ -1,14 +1,15 @@
 {
   description = "11-dev-wsl2";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-untable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
 }
