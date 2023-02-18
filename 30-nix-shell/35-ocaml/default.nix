45304e96ac283683ab0486b51c94676499764a7f
mingyuchoo <mingyuchoo@gmail.com>
2023-02-18 17:52:03 +0900

    added

---
+++ 30-nix-shell/35-ocaml/default.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "ocaml";
+    buildInputs = [
+      pkgs.ocaml-ng.ocamlPackages_latest.ocaml
+      pkgs.ocaml-ng.ocamlPackages_latest.findlib
+      pkgs.ocaml-ng.ocamlPackages_latest.dune_3
+      pkgs.ocaml-ng.ocamlPackages_latest.ocaml-lsp
+      pkgs.ocaml-ng.ocamlPackages_latest.ocamlgraph
+    ];
+    shellHook = ''
+      echo "Welcome to nix-shell for OCaml!"
+    '';
+  }
