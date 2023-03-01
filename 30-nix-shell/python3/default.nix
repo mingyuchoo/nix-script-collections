b2e7204f2be638781be272baf39f2d187201dfa1
mingyuchoo <mingyuchoo@gmail.com>
2023-03-01 16:52:00 +0900

    Update default.nix

--- 30-nix-shell/python3/default.nix
+++ 30-nix-shell/python3/default.nix
@@ -12,6 +12,8 @@ in
     shellHook = ''
       export EDITOR=emacs
       eval "$(direnv hook bash)"
+      python -m venv venv
+      source venv/bin/activate
       echo "Welcome to nix-shell for Python3!"
     '';
   }
