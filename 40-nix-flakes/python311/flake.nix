ac59fd62ac7cbaceab05755fa0633522719dabe8
mingyuchoo <mingyuchoo@gmail.com>
2024-02-07 13:42:31 +0900

    updated

--- 40-nix-flakes/python311/flake.nix
+++ 40-nix-flakes/python311/flake.nix
@@ -1,39 +1,51 @@
 {
-  description = "A basic devShell using flake-utils for Python3";
+  description = "A basic devShell using nixpkgs for Python311";
 
   inputs = {
-    # flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
   outputs = { self, nixpkgs, }:
     let
-      allSystems = [
+      systems = [
         "aarch64-linux"
         "aarch64-darwin"
       ];
 
       forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
+        nixpkgs.lib.genAttrs systems
           (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
       {
         devShells = forAllSystems ({ pkgs }: {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              direnv
-              python311Full
-              python311Packages.pip
-              python311Packages.virtualenv
-            ];
-            shellHook = ''
-              export LANG=C.UTF-8
-              export EDITOR=emacs
-              export PS1="\\e[1;34m(develop) \u@\h:\W > \\e[0m"
-              eval "$(direnv hook bash)"
-              echo "Welcome to nix flake for Python3"
-            '';
+          default =
+            let
+            in
+              pkgs.mkShell {
+                buildInputs = with pkgs; [
+                  direnv
+                  python311Full
+                  python311Packages.pip
+                  python311Packages.virtualenv
+                ];
+                shellHook = ''
+                  export LANG=C.UTF-8
+                  export EDITOR=emacs
+                  export PS1="\\e[1;34m(develop) \u@\h:\W > \\e[0m"
+                  eval "$(direnv hook bash)"
+                  echo "Welcome to nix flake for Python3"
+                '';
           };
+        });
+        packages = forAllSystems ({ pkgs }: {
+          default =
+            let
+            in
+              pkgs.stdenv.mkDerivation {
+                name = "python311_app";
+                pname = "python311_app";
+                dontBuild = true;
+              };
         });
       };
 }
