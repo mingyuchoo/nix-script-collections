1b7ba2f7ea15487e7275819686a2b4454c0c4ea7
mingyuchoo <mingyuchoo@gmail.com>
2024-03-06 04:56:12 +0900

    updated home.nix

--- 00-nixos/home/my_user_name/.config/home-manager/home.nix
+++ 00-nixos/home/my_user_name/.config/home-manager/home.nix
@@ -5,57 +5,13 @@
   home.homeDirectory = "/home/mgch";
   home.stateVersion = "23.11";
   home.packages = [
-    pkgs.kate
-    pkgs.firefox
-    pkgs.microsoft-edge
-    pkgs.htop
-    pkgs.jq
-    pkgs.neovim
-    pkgs.netcat
-    pkgs.nnn
-    pkgs.tmux
-    pkgs.tree
-    pkgs.unzip
-    pkgs.wget
-    pkgs.xsel
-    pkgs.zip
-    pkgs.vscode
-    pkgs.maestral
-    pkgs.maestral-gui
-    pkgs.discord
-    pkgs.drawio
-    pkgs.slack
-    pkgs.telegram-desktop
-    pkgs.zoom-us
     pkgs.bun
-    pkgs.clang
     pkgs.cargo-modules
     pkgs.cargo-watch
-    pkgs.rustup
-    pkgs.nodejs-18_x
-    pkgs.nodePackages_latest.pnpm
-    pkgs.nodePackages_latest.typescript
-    pkgs.gradle
-    pkgs.jdk17
-    pkgs.haskell.compiler.ghc963
-    pkgs.haskell.packages.ghc963.cabal-install
-    pkgs.haskell.packages.ghc963.ghcid
-    pkgs.haskell.packages.ghc963.haskell-language-server
-    pkgs.haskell.packages.ghc963.hlint
-    pkgs.haskell.packages.ghc963.hoogle
-    pkgs.haskell.packages.ghc963.stylish-haskell
-    pkgs.haskellPackages.stack
-    pkgs.haskellPackages.hindent
-    pkgs.haskellPackages.dhall
-    pkgs.haskellPackages.dhall-bash
-    pkgs.haskellPackages.dhall-json
-    pkgs.haskellPackages.dhall-nix
-    pkgs.haskellPackages.dhall-toml
-    pkgs.haskellPackages.dhall-yaml
-    pkgs.ocaml-ng.ocamlPackages_latest.ocaml
-    pkgs.ocaml-ng.ocamlPackages_latest.ocamlgraph
-    pkgs.ocaml-ng.ocamlPackages_latest.dune_3
-    pkgs.ocaml-ng.ocamlPackages_latest.findlib
+    pkgs.clang
+    pkgs.direnv
+    pkgs.discord
+    pkgs.drawio
     pkgs.elmPackages.elm
     pkgs.elmPackages.elm-analyse
     pkgs.elmPackages.elm-doc-preview
@@ -63,59 +19,69 @@
     pkgs.elmPackages.elm-language-server
     pkgs.elmPackages.elm-live
     pkgs.elmPackages.elm-test
-    pkgs.lua5_4
+    pkgs.emacs29-gtk3
+    pkgs.firefox
+    pkgs.git
+    pkgs.gnumake
+    pkgs.gradle
+    pkgs.haskell.compiler.ghc963
+    pkgs.haskell.packages.ghc963.cabal-install
+    pkgs.haskell.packages.ghc963.ghcid
+    pkgs.haskell.packages.ghc963.haskell-language-server
+    pkgs.haskell.packages.ghc963.hlint
+    pkgs.haskell.packages.ghc963.hoogle
+    pkgs.haskell.packages.ghc963.stylish-haskell
+    pkgs.haskellPackages.dhall
+    pkgs.haskellPackages.dhall-bash
+    pkgs.haskellPackages.dhall-json
+    pkgs.haskellPackages.dhall-nix
+    pkgs.haskellPackages.dhall-toml
+    pkgs.haskellPackages.dhall-yaml
+    pkgs.haskellPackages.hindent
+    pkgs.haskellPackages.stack
+    pkgs.htop
+    pkgs.jdk17
+    pkgs.jq
+    pkgs.kate
+    pkgs.libreoffice
     pkgs.lua-language-server
+    pkgs.lua5_4
+    pkgs.maestral
+    pkgs.maestral-gui
+    pkgs.microsoft-edge
+    pkgs.neovim
+    pkgs.netcat
+    pkgs.nnn
+    pkgs.nodePackages_latest.pnpm
+    pkgs.nodePackages_latest.typescript
+    pkgs.nodejs-18_x
+    pkgs.ocaml-ng.ocamlPackages_latest.dune_3
+    pkgs.ocaml-ng.ocamlPackages_latest.findlib
+    pkgs.ocaml-ng.ocamlPackages_latest.ocaml
+    pkgs.ocaml-ng.ocamlPackages_latest.ocamlgraph
     pkgs.python3
     pkgs.python3Packages.pip
     pkgs.python3Packages.virtualenv
+    pkgs.rustup
+    pkgs.slack
+    pkgs.telegram-desktop
+    pkgs.tmux
+    pkgs.tree
+    pkgs.unzip
+    pkgs.vscode
+    pkgs.wget
+    pkgs.xsel
     pkgs.zig
+    pkgs.zip
+    pkgs.zoom-us
   ];
   home.file = {
   };
   home.sessionVariables = {
     EDITOR = "emacs";
   };
-  home.shellAliases = {
-    "..." = "cd ../..";
-  };
   nixpkgs.config = {
     allowUnfree = true;
   };
   programs.home-manager.enable = true;
-    programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-  };
-  programs.emacs = {
-    enable = true;
-    extraConfig = ''
-      (setq standard-indent 2)
-    '';
-    extraPackages = epkgs: [
-      epkgs.nix-mode
-      epkgs.magit
-    ];
-  };
-  programs.firefox.enable = true;
-  programs.git = {
-    enable = true;
-    aliases = {
-      ls = "log --graph --pretty=onelone";
-    };
-    attributes = [
-    ];
-    delta.enable = true;
-    extraConfig = {
-      core = {
-        editor = "emacs";
-        autocrlf = "input";
-        longpaths = true;
-      };
-      credential = {
-        helper = "store";
-      };
-    };
-    userName = "Mingyu Choo";
-    userEmail = "mingyuchoo@gmail.com";
-  };
 }
