5895b640d2c33c97403a777b1f2f4c1ed5408418
mingyuchoo <mingyuchoo@gmail.com>
2024-08-01 04:46:24 +0900

    refined

--- 00-nixos/home/my_user_name/.config/home-manager/home.nix
+++ 00-nixos/home/my_user_name/.config/home-manager/home.nix
@@ -4,94 +4,102 @@
   home.username = "mgch";
   home.homeDirectory = "/home/mgch";
   home.stateVersion = "23.11";
-  home.packages = [
-    pkgs.beekeeper-studio
-    pkgs.brave
-    pkgs.bun
-    pkgs.cargo-modules
-    pkgs.cargo-watch
-    pkgs.clang
-    pkgs.cmake
-    pkgs.dbeaver
-    pkgs.direnv
-    pkgs.discord
-    pkgs.drawio
-    pkgs.elixir_1_16
-    pkgs.elixir-ls
-    pkgs.elmPackages.elm
-    pkgs.elmPackages.elm-analyse
-    pkgs.elmPackages.elm-doc-preview
-    pkgs.elmPackages.elm-format
-    pkgs.elmPackages.elm-language-server
-    pkgs.elmPackages.elm-live
-    pkgs.elmPackages.elm-test
-    pkgs.emacs29-gtk3
-    pkgs.erlang_26
-    pkgs.fuse3
-    pkgs.git
-    pkgs.gnumake
-    pkgs.gradle
-    pkgs.haskell.compiler.ghc963
-    pkgs.haskell.packages.ghc963.cabal-install
-    pkgs.haskell.packages.ghc963.ghcid
-    pkgs.haskell.packages.ghc963.haskell-language-server
-    pkgs.haskell.packages.ghc963.hlint
-    pkgs.haskell.packages.ghc963.hoogle
-    pkgs.haskell.packages.ghc963.stylish-haskell
-    pkgs.haskellPackages.dhall
-    pkgs.haskellPackages.dhall-bash
-    pkgs.haskellPackages.dhall-json
-    pkgs.haskellPackages.dhall-nix
-    pkgs.haskellPackages.dhall-toml
-    pkgs.haskellPackages.dhall-yaml
-    pkgs.haskellPackages.hindent
-    pkgs.haskellPackages.stack
-    pkgs.htop
-    pkgs.hurl
-    pkgs.ifuse
-    pkgs.inotify-tools
-    pkgs.jdk17
-    pkgs.jq
-    pkgs.kate
-    pkgs.lazydocker
-    pkgs.libimobiledevice
-    pkgs.libreoffice
-    pkgs.lua-language-server
-    pkgs.lua5_4
-    pkgs.maestral
-    pkgs.maestral-gui
-    pkgs.microsoft-edge
-    pkgs.neovim
-    pkgs.netcat
-    pkgs.nnn
-    pkgs.nodePackages_latest.pnpm
-    pkgs.nodePackages_latest.typescript
-    pkgs.nodejs-18_x
-    pkgs.ocaml-ng.ocamlPackages_latest.dune_3
-    pkgs.ocaml-ng.ocamlPackages_latest.findlib
-    pkgs.ocaml-ng.ocamlPackages_latest.ocaml
-    pkgs.ocaml-ng.ocamlPackages_latest.ocamlgraph
-    pkgs.openssl
-    pkgs.packer
-    pkgs.pkg-config
-    pkgs.python3
-    pkgs.python3Packages.pip
-    pkgs.python3Packages.virtualenv
-    pkgs.ranger
-    pkgs.rustup
-    pkgs.slack
-    pkgs.telegram-desktop
-    pkgs.terraform
-    pkgs.tmux
-    pkgs.tree
-    pkgs.unzip
-    pkgs.vscode
-    pkgs.wget
-    pkgs.xsel
-    pkgs.yarn
-    pkgs.zig
-    pkgs.zip
-    pkgs.zoom-us
+  home.packages = with pkgs; [
+    # c/c++
+    clang
+    clang-tools
+    cmake
+    gnumake
+    lld
+    llvm
+    # bun
+    bun
+    # elixir 
+    elixir_1_16
+    elixir-ls
+    erlang_26
+    # elm
+    elmPackages.elm
+    elmPackages.elm-analyse
+    elmPackages.elm-doc-preview
+    elmPackages.elm-format
+    elmPackages.elm-language-server
+    elmPackages.elm-live
+    elmPackages.elm-test
+    # haskell
+    haskell.compiler.ghc963
+    haskell.packages.ghc963.cabal-install
+    haskell.packages.ghc963.ghcid
+    haskell.packages.ghc963.haskell-language-server
+    haskell.packages.ghc963.hlint
+    haskell.packages.ghc963.hoogle
+    haskell.packages.ghc963.stylish-haskell
+    haskellPackages.dhall
+    haskellPackages.dhall-bash
+    haskellPackages.dhall-json
+    haskellPackages.dhall-nix
+    haskellPackages.dhall-toml
+    haskellPackages.dhall-yaml
+    haskellPackages.hindent
+    haskellPackages.stack
+    # java
+    gradle
+    jdk17
+    # lua
+    lua-language-server
+    lua5_4
+    # ocaml
+    opam
+    # node
+    nodePackages_latest.pnpm
+    nodePackages_latest.typescript
+    nodejs-18_x
+    yarn
+    # python
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    # rust
+    rustup
+    # zig
+    zig
+    # lib
+    ifuse
+    inotify-tools
+    libimobiledevice
+    libreoffice
+    openssl
+    pkg-config
+    # CLI
+    direnv
+    emacs29-gtk3
+    fuse3
+    git
+    htop
+    hurl
+    jq
+    lazydocker
+    neovim
+    netcat
+    packer
+    terraform
+    tmux
+    tree
+    unzip
+    wget
+    xsel
+    zip
+    # GUI
+    dbeaver
+    discord
+    drawio
+    maestral
+    maestral-gui
+    microsoft-edge
+    slack
+    telegram-desktop
+    vscode
+    zoom-us
   ];
   home.file = {
   };
